50a30db37887e16cf063f1e888d606d33be9e260
John Hewson <john@jahewson.com>
2012-07-13 19:40:09 +0100

    stack traces and version numbers

--- ocaml/src/Csm2Mzn.ml
+++ ocaml/src/Csm2Mzn.ml
@@ -20,16 +20,17 @@ let rec printTokens (lexbuf) =
   
 (* parses command-line args imperatively *)
 let main () =
-  let (filename, showTokens, showAst, showCounting, hasComments) = 
-      ((ref ""), (ref false), (ref false), (ref false), (ref false)) 
+  let (filename, showTokens, showAst, showCounting, hasComments, showVersion) = 
+      ((ref ""), (ref false), (ref false), (ref false), (ref false), (ref false)) 
   in
   let arglist = [
     ("-c", Arg.Set hasComments, "comment the MiniZinc");
+    ("-v", Arg.Set showVersion, "print version");
     ("--debug-tokens", Arg.Set showTokens, "print lexer tokens (debug)");
     ("--debug-ast", Arg.Set showAst, "print AST (debug)");
     ("--debug-counting", Arg.Set showCounting, "print object count (debug)")] 
   in
-  let msg = "usage: filename [options]" in
+    let msg = "usage: filename [options]" in
   let _ = (Arg.parse arglist (fun s ->
       if String.length !filename = 0 then
         filename := s
@@ -37,36 +38,41 @@ let main () =
         raise (Arg.Bad (sprintf "unexpected argument '%s'" s))
       ) msg)
   in
-  if String.length !filename = 0 then
-    (Arg.usage arglist msg;
-    exit 1)
+  if !showVersion then
+    (print_endline "(C) 2011-2012 The University Court of the University of Edinburgh";
+     print_endline ("Version 2.0a @ " ^ Version.sha);
+    exit 0)
   else
-    let lexbuf = Lexing.from_channel (open_in !filename) in
-    if !showTokens then
-      printTokens lexbuf
+    if String.length !filename = 0 then
+      (Arg.usage arglist msg;
+      exit 1)
     else
-      let ast =
-        try
-          Parser.model Lexer.token lexbuf
-        with
-        | Parsing.Parse_error ->
-          let tok = Lexing.lexeme lexbuf in
-          let curr = lexbuf.lex_curr_p in
-          let cnum = curr.pos_cnum - curr.pos_bol in
-          let line = curr.pos_lnum in
-          print_endline ("File \"" ^ !filename ^ "\", line " ^ string_of_int line ^ " character " ^ string_of_int cnum ^
-                         ":\nError: Syntax error at `" ^ tok ^ "`");
-          exit 1
-        in
-          if !showAst then
-            Debug.printAst ast
-          else
-            let ast = resolveForwardDecls ast in
-            typeCheck ast;
-            let ast = decomposeLiterals ast in
-            let mz = toMiniZinc ast !showCounting !hasComments in
-            print_endline mz
-      ;;
+      let lexbuf = Lexing.from_channel (open_in !filename) in
+      if !showTokens then
+        printTokens lexbuf
+      else
+        let ast =
+          try
+            Parser.model Lexer.token lexbuf
+          with
+          | Parsing.Parse_error ->
+            let tok = Lexing.lexeme lexbuf in
+            let curr = lexbuf.lex_curr_p in
+            let cnum = curr.pos_cnum - curr.pos_bol in
+            let line = curr.pos_lnum in
+            print_endline ("File \"" ^ !filename ^ "\", line " ^ string_of_int line ^ " character " ^ string_of_int cnum ^
+                           ":\nError: Syntax error at `" ^ tok ^ "`");
+            exit 1
+          in
+            if !showAst then
+              Debug.printAst ast
+            else
+              let ast = resolveForwardDecls ast in
+              typeCheck ast;
+              let ast = decomposeLiterals ast in
+              let mz = toMiniZinc ast !showCounting !hasComments in
+              print_endline mz
+        ;;
       
 Printexc.record_backtrace true;;
 main();;
